63e70412737e8532a61ad4955998400dee027bdd
sbadajoz <sbadajoz@student.ccd.edu>
2017-10-10 16:45:48 -0700

    Fix null pointer bug

--- app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/SettingsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/SettingsActivity.java
@@ -10,7 +10,6 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
